0249daceeba528dafbeaa531f825de05dfffa763
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2017-11-08 20:50:44 +0200

    Extract Content wrapper from layout

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
 import Header from '../components/Header'
+import Content from '../components/Content'
 
 import './index.css'
 
@@ -10,16 +11,7 @@ const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet title="anna.eevert.love" />
     <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
+    <Content>{children()}</Content>
   </div>
 )
 
