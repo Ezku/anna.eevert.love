01b0d892ad445f4e61b6b204c0ce850c878d4c93
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2017-12-02 20:20:43 +0200

    Render rsvp hero image

--- src/pages/index/Rsvp.js
+++ src/pages/index/Rsvp.js
@@ -1,5 +1,12 @@
 import React from 'react'
 
+import hero from './rsvp-hero.jpg'
+
 export default function Rsvp() {
-  return <div>rsvp pls</div>
+  return (
+    <div>
+      <img src={hero} />
+      rsvp pls
+    </div>
+  )
 }
