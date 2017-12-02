5db741f019a98c251f9eb8618053e5ceb6f5abaf
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2017-12-02 19:12:31 +0200

    Render header with react-headroom

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,36 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import Headroom from 'react-headroom'
 
 const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
+  <Headroom>
     <div
       style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
+        background: 'rebeccapurple',
+        marginBottom: '1.45rem',
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          Gatsby
-        </Link>
-      </h1>
+      <div
+        style={{
+          margin: '0 auto',
+          maxWidth: 960,
+          padding: '1.45rem 1.0875rem',
+        }}
+      >
+        <h1 style={{ margin: 0 }}>
+          <Link
+            to="/"
+            style={{
+              color: 'white',
+              textDecoration: 'none',
+            }}
+          >
+            Gatsby
+          </Link>
+        </h1>
+      </div>
     </div>
-  </div>
+  </Headroom>
 )
 
 export default Header
