e596191f20e8e4dbcf089866c530f84177ad4716
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2017-12-02 19:51:24 +0200

    Render header with green on white

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,20 +2,32 @@ import React from 'react'
 import Link from 'gatsby-link'
 import Headroom from 'react-headroom'
 import glamorous from 'glamorous'
+import scale from '../utils/scale'
 
 const Background = glamorous.div({
-  background: 'rebeccapurple',
-  marginBottom: '1.45rem',
+  background: 'white',
+  boxShadow: '0 1px rgba(0, 0, 0, 0.1), 0 2px rgba(0, 0, 0, 0.05)',
 })
 
 const Content = glamorous.div({
   margin: '0 auto',
   maxWidth: 960,
-  padding: '1.45rem 1.0875rem',
+  padding: `${scale(0)}px ${scale(0)}px`,
 })
 
 const Home = glamorous.h1({
   margin: 0,
+  fontSize: '1rem',
+})
+
+const MainLink = glamorous(Link)({
+  color: '#28a745',
+  textDecoration: 'none',
+  ':visited': {
+    color: '#28a745',
+  },
+  fontSize: `${scale(2)}px`,
+  lineHeight: `${scale(3)}px`,
 })
 
 const Header = () => (
@@ -23,15 +35,7 @@ const Header = () => (
     <Background>
       <Content>
         <Home>
-          <Link
-            to="/"
-            style={{
-              color: 'white',
-              textDecoration: 'none',
-            }}
-          >
-            Gatsby
-          </Link>
+          <MainLink to="/">anna.eevert.love</MainLink>
         </Home>
       </Content>
     </Background>
