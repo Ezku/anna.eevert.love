fb24a6ecb31753231f7f62ad1bb98b38fe9aaf9b
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2017-12-02 21:29:52 +0200

    Style footer

--- src/pages/index/Contact.js
+++ src/pages/index/Contact.js
@@ -1,12 +1,46 @@
 import React from 'react'
+import glamorous from 'glamorous'
+
 import Content from '../../components/Content'
+import scale from '../../utils/scale'
+
+const Footer = glamorous.footer({})
+
+const Background = glamorous.div({
+  backgroundColor: 'rgba(0,0,0,0.88)',
+  borderTop: '3px double white',
+  paddingTop: `${scale(4 + 3) / 2 - 3}px`,
+  minHeight: '256px',
+})
+
+const Header = glamorous.header({
+  textAlign: 'center',
+  marginBottom: `-${scale(4 + 3) / 2 - 3}px`,
+})
+
+const H2 = glamorous.h2({
+  display: 'inline-block',
+  fontSize: `${scale(4)}px`,
+  lineHeight: `${scale(4)}px`,
+  background: 'white',
+  padding: `${scale(1)}px ${scale(0)}px`,
+  border: '3px double black',
+})
+
+const Section = glamorous.section({
+  color: '#f8f9fa',
+  letterSpacing: '2px',
+})
 
 export default function Contact() {
   return (
-    <footer>
-      <Content>
-        <h2>Yhteystiedot</h2>
-      </Content>
-    </footer>
+    <Footer>
+      <Header>
+        <H2>Yhteystiedot</H2>
+      </Header>
+      <Background>
+        <Content />
+      </Background>
+    </Footer>
   )
 }
