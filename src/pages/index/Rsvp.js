945403db8aed64102ba6c1f0ec895dae8f9698d0
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2017-12-02 20:39:20 +0200

    Add wrapper around hero to constrain height

--- src/pages/index/Rsvp.js
+++ src/pages/index/Rsvp.js
@@ -1,12 +1,22 @@
 import React from 'react'
+import glamorous from 'glamorous'
+
 import Content from '../../components/Content'
 
 import hero from './rsvp-hero.jpg'
 
+const Hero = glamorous.img()
+const HeroContainer = glamorous.div({
+  maxHeight: '512px',
+  overflowY: 'hidden',
+})
+
 export default function Rsvp() {
   return (
     <div>
-      <img src={hero} />
+      <HeroContainer>
+        <Hero src={hero} />
+      </HeroContainer>
       <Content>rsvp pls</Content>
     </div>
   )
