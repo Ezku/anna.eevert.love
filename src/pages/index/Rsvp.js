fd16821e39dd82f64a5b4c7fc0fa54fb5a9fde27
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2017-12-02 21:15:26 +0200

    Style RSVP header

--- src/pages/index/Rsvp.js
+++ src/pages/index/Rsvp.js
@@ -2,6 +2,7 @@ import React from 'react'
 import glamorous from 'glamorous'
 
 import Content from '../../components/Content'
+import scale from '../../utils/scale'
 
 import hero from './rsvp-hero.jpg'
 
@@ -11,13 +12,31 @@ const HeroContainer = glamorous.div({
   maxHeight: '512px',
   background: `url(${hero}) 50% 50% no-repeat`,
   backgroundSize: 'cover',
+  backgroundClip: 'content-box',
+  borderBottom: '3px double black',
+})
+
+const Header = glamorous.header({
+  textAlign: 'center',
+  marginTop: `-${3 + scale(4 + 2 + 1) / 2}px`,
+})
+
+const H2 = glamorous.h2({
+  display: 'inline-block',
+  fontSize: `${scale(4)}px`,
+  lineHeight: `${scale(4)}px`,
+  background: 'white',
+  padding: `${scale(1)}px ${scale(0)}px`,
+  border: '3px double black',
 })
 
 export default function Rsvp() {
   return (
-    <div>
+    <section>
       <HeroContainer />
-      <Content>rsvp pls</Content>
-    </div>
+      <Header>
+        <H2>RSVP</H2>
+      </Header>
+    </section>
   )
 }
