11027ce725919be5a84233545989762745f36897
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2018-01-09 20:59:25 +0200

    Add RSVP buttons to day and night event signup

--- src/pages/index/Rsvp.js
+++ src/pages/index/Rsvp.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import glamorous from 'glamorous'
+import { Container, Row, Col } from 'glamorous-grid'
 
 import Content from '../../components/Content'
 import scale from '../../utils/scale'
@@ -11,6 +12,7 @@ const Section = glamorous.section({
   paddingTop: '117px',
   marginTop: '-117px',
   minHeight: '100vh',
+  paddingBottom: `${scale(5)}px`
 })
 
 const HeroContainer = glamorous.div({
@@ -37,6 +39,31 @@ const H2 = glamorous.h2({
   border: '3px double black',
 })
 
+const Lede = glamorous.p({
+  marginBottom: `${scale(0)}px`
+})
+
+const ButtonLink = glamorous.a({
+  display: 'inline-block',
+  padding: `${scale(2)}px ${scale(1)}px`,
+  margin: `${scale(0)}px ${scale(0)}px`,
+  background: 'white',
+  textDecoration: 'none',
+  borderBottom: '3px double black',
+  fontWeight: 'bold',
+  color: 'rgba(0, 0, 0, 0.8)',
+  borderRadius: '3px',
+  boxShadow: '0 2px 3px 0 hsla(0,0%,4%,.1), 0 0 5px 0 hsla(0,0%,4%,.05)',
+  transition: 'box-shadow ease-out 0.166s',
+  ':hover': {
+    boxShadow: '0 2px 6px 0 hsla(0,0%,4%,.2), 0 0 10px 0 hsla(0,0%,4%,.1)',
+  }
+})
+
+const RsvpColumn = glamorous(Col)({
+  textAlign: 'center'
+})
+
 export default function Rsvp() {
   return (
     <Section id="rsvp">
@@ -44,6 +71,25 @@ export default function Rsvp() {
       <Header>
         <H2>RSVP</H2>
       </Header>
+      <Container>
+        <Row justifyContent="center">
+          <RsvpColumn span={{ xs: 1, md: 8/12, lg: 6/12 }}>
+            <Lede>Oletko saapumassa päiväjuhlaan (alkaen klo 14) vai iltajuhlaan (alkaen klo 19)?</Lede>
+          </RsvpColumn>
+        </Row>
+        <Row justifyContent="center">
+          <RsvpColumn span={{ sm: 6/12, md: 5/12 }}>
+            <ButtonLink href="https://paiva.rsvp.anna.eevert.love">
+              Ilmoittaudun päiväjuhlaan
+            </ButtonLink>
+          </RsvpColumn>
+          <RsvpColumn span={{ sm: 6/12, md: 5/12 }}>
+            <ButtonLink href="https://ilta.rsvp.anna.eevert.love">
+              Ilmoittaudun iltajuhlaan
+            </ButtonLink>
+          </RsvpColumn>
+        </Row>
+      </Container>
     </Section>
   )
 }
