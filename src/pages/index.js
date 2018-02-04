3445ac7adb6a8a87c1382e9f52a3281dc9bec612
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2018-02-04 21:38:17 +0200

    Reconvert all hero images

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,9 @@ import React from 'react'
 import Rsvp from './index/Rsvp'
 import Contact from './index/Contact'
 
-import rsvpHero from './index/rsvp-hero-color.jpg'
+import hero1 from './index/hero-8650.jpg'
+import hero2 from './index/hero-8667.jpg'
+import hero3 from './index/hero-8675.jpg'
 
 import Header from '../components/Header'
 import HeroSection from '../components/HeroSection'
@@ -11,7 +13,7 @@ import Footer from '../components/Footer'
 export default () => (
   <div>
     <Header />
-    <HeroSection id="rsvp" header="RSVP" hero={rsvpHero}>
+    <HeroSection id="rsvp" header="RSVP" hero={hero3}>
       <Rsvp />
     </HeroSection>
     <Footer id="yhteystiedot" header="Yhteystiedot">
