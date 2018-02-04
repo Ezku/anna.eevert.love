20ba941498912b23f01146adf112fd366422131b
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2018-02-04 18:01:50 +0200

    Add content for contact section

--- src/pages/index/Contact.js
+++ src/pages/index/Contact.js
@@ -20,24 +20,30 @@ const Header = glamorous.header({
   marginBottom: `-${scale(9) / 2 - 3}px`,
 })
 
-const H2 = glamorous.h2({
-  display: 'inline-block',
-  background: 'white',
-  padding: `${scale(1)}px ${scale(0)}px`,
-  border: '3px double black',
-}, ...headers.h2)
+const H2 = glamorous.h2(
+  {
+    display: 'inline-block',
+    background: 'white',
+    padding: `${scale(1)}px ${scale(0)}px`,
+    border: '3px double black',
+  },
+  ...headers.h2
+)
 
 const Section = glamorous.section({
   color: '#f8f9fa',
   letterSpacing: '2px',
 })
 
-const H3 = glamorous.h3({
-  display: 'block',
-  padding: `${scale(-3)}px ${scale(-5)}px`,
-  borderBottom: '3px double black',
-  marginBottom: `${scale(0)}px`
-}, ...headers.h3)
+const H3 = glamorous.h3(
+  {
+    display: 'block',
+    padding: `${scale(-3)}px ${scale(-5)}px`,
+    borderBottom: '3px double black',
+    marginBottom: `${scale(0)}px`,
+  },
+  headers.h3
+)
 
 const ContactColumn = glamorous(Col)({
   margin: `${scale(0)}px ${scale(0)}px`,
@@ -48,8 +54,25 @@ const ContactColumn = glamorous(Col)({
 })
 
 const P = glamorous.p({
-  marginBottom: `${scale(1)}px`
+  marginBottom: `${scale(0)}px`,
 })
+
+const DL = glamorous.dl({
+  marginBottom: `${scale(0)}px`,
+})
+
+const DT = glamorous.dt({
+  fontWeight: 'bold',
+  display: 'inline-block',
+})
+
+const DD = glamorous.dd({
+  margin: 0,
+  textIndent: '2ch',
+})
+
+const A = glamorous.a({})
+const Mailto = ({ children }) => <A href={`mailto:${children}`}>{children}</A>
 
 export default function Contact() {
   return (
@@ -61,13 +84,42 @@ export default function Contact() {
         <Content>
           <Container>
             <Row justifyContent="center">
-              <ContactColumn span={{ sm: 10/12, md: 5/12 }}>
+              <ContactColumn span={{ sm: 10 / 12, md: 5 / 12 }}>
                 <H3>Hääpari</H3>
-                <P>Juttuihin X ja Y koskevissa tiedusteluissa ota yhteyttä</P>
+                <P>
+                  Ennakkotiedustelut järjestelyistä, lahjoista, aikataulusta jne, ellet halua
+                  yllättää.
+                </P>
+                <DL>
+                  <DT>Anna</DT>
+                  <DD>
+                    <Mailto>anna.m.torronen@gmail.com</Mailto>
+                  </DD>
+                  <DD>050-533-5993</DD>
+                </DL>
+                <DL>
+                  <DT>Eevert</DT>
+                  <DD>
+                    <Mailto>anna.m.torronen@gmail.com</Mailto>
+                  </DD>
+                  <DD>040-508-2297</DD>
+                </DL>
               </ContactColumn>
-              <ContactColumn span={{ sm: 10/12, md: 5/12 }}>
+              <ContactColumn span={{ sm: 10 / 12, md: 5 / 12 }}>
                 <H3>Hääesikunta</H3>
-                <P>Juttuihin Z tai W koskevissa tiedusteluissa ota yhteyttä</P>
+                <P>Kun haluat yllättää, tai jos hääpäivänä iskee hätä. :)</P>
+                <DL>
+                  <DT>Best Lady</DT>
+                  <DD>
+                    <Mailto>anna.torvinen@gmail.com</Mailto>
+                  </DD>
+                </DL>
+                <DL>
+                  <DT>Best Man</DT>
+                  <DD>
+                    <Mailto>otso.hannula@gmail.com</Mailto>
+                  </DD>
+                </DL>
               </ContactColumn>
             </Row>
           </Container>
