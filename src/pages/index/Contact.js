18b3d3376e28cf16b8326f84a2df0b718ea387b6
Eevert Saukkokoski <eevert.saukkokoski@gmail.com>
2018-02-11 21:48:11 +0200

    Consolidate Contact styles with Typography

--- src/pages/index/Contact.js
+++ src/pages/index/Contact.js
@@ -3,17 +3,12 @@ import glamorous from 'glamorous'
 import { Container, Row, Col } from 'glamorous-grid'
 
 import scale from '../../utils/scale'
-import { headers } from '../../utils/typography'
+import { Section, H3, P, A } from '../../components/Typography'
 
-const H3 = glamorous.h3(
-  {
-    display: 'block',
-    padding: `${scale(-3)}px ${scale(-5)}px`,
-    borderBottom: '3px double black',
-    marginBottom: `${scale(0)}px`,
-  },
-  headers.h3
-)
+const FullWidthH3 = glamorous(H3)({
+  display: 'block',
+  borderBottom: '3px double black',
+})
 
 const ContactCol = glamorous(Col)({
   marginBottom: `${scale(2)}px`,
@@ -26,10 +21,6 @@ const ContactBox = glamorous.div({
   color: 'rgba(0, 0, 0, 0.8)',
   borderRadius: '3px',
   boxShadow: '0 5px 15px 0 hsla(0,0%,4%,.9), 0 0 15px 0 hsla(0,0%,4%,.45)',
-})
-
-const P = glamorous.p({
-  marginBottom: `${scale(0)}px`,
 })
 
 const DL = glamorous.dl({
@@ -50,7 +41,6 @@ const DD = glamorous.dd({
   wordWrap: 'normal',
 })
 
-const A = glamorous.a({})
 const Mailto = ({ children }) => <A href={`mailto:${children}`}>{children}</A>
 
 export default function Contact({ Footer }) {
@@ -59,7 +49,7 @@ export default function Contact({ Footer }) {
       <Row justifyContent="center">
         <ContactCol span={{ sm: 10 / 12, md: 6 / 12, lg: 5 / 12 }}>
           <ContactBox>
-            <H3>Hääpari</H3>
+            <FullWidthH3>Hääpari</FullWidthH3>
             <P>
               Ennakkotiedustelut järjestelyistä, lahjoista, aikataulusta jne, ellet halua yllättää.
             </P>
@@ -81,7 +71,7 @@ export default function Contact({ Footer }) {
         </ContactCol>
         <ContactCol span={{ sm: 10 / 12, md: 6 / 12, lg: 5 / 12 }}>
           <ContactBox>
-            <H3>Hääesikunta</H3>
+            <FullWidthH3>Hääesikunta</FullWidthH3>
             <P>Kun haluat yllättää, tai jos hääpäivänä iskee hätä. :)</P>
             <DL>
               <DT>Best Lady</DT>
